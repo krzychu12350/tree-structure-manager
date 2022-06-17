7f6df8362902704d040df305b26b352a57f4036f
Krzysztof Karaś <krzychu12350@interia.pl>
2022-06-18 01:40:13 +0200

    Last little code refactoring - improved cleanliness

--- resources/views/layouts/treeManagementView.blade.php
+++ resources/views/layouts/treeManagementView.blade.php
@@ -14,7 +14,6 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-
                         <div class="row">
                             @if(auth()->user()->isAdmin())
                                 <div id="tree" class="tree well col-md-8"></div>
@@ -22,41 +21,43 @@
                                     <div class="container">
                                         <ul class="nav nav-pills">
                                             <li class="nav-item">
-                                                <a class="nav-link active" id="add-tab" data-bs-toggle="pill" href="#tab-pane">Add</a>
+                                                <a class="nav-link active" id="add-tab" data-bs-toggle="pill"
+                                                   href="#tab-pane">Add</a>
                                             </li>
                                             <li class="nav-item">
-                                                <a class="nav-link" id="edit-tab" data-bs-toggle="pill" href="#tab-edit">Edit</a>
+                                                <a class="nav-link" id="edit-tab" data-bs-toggle="pill"
+                                                   href="#tab-edit">Edit</a>
                                             </li>
                                             <li class="nav-item">
-                                                <a class="nav-link" id="delete-tab" data-bs-toggle="pill" href="#tab-delete">Delete</a>
+                                                <a class="nav-link" id="delete-tab" data-bs-toggle="pill"
+                                                   href="#tab-delete">Delete</a>
                                             </li>
                                         </ul>
                                         <div class="tab-content">
                                             <div class="tab-pane container active p-0" id="tab-pane">
-
-
                                                 <div class="mt-4">
                                                     <h2>Add new a node or a leaf</h2>
                                                     <form id="addCategoryForm" class="needs-validation">
                                                         <div class="mb-3">
                                                             <label class="form-label" for="inputNameAdd">Name</label>
-                                                            <input type="text" class="form-control" id="inputNameAdd" placeholder="Name" required>
-                                                            <div class="invalid-feedback">Please enter a valid name.</div>
+                                                            <input type="text" class="form-control" id="inputNameAdd"
+                                                                   placeholder="Name" required>
+                                                            <div class="invalid-feedback">Please enter a valid name.
+                                                            </div>
                                                         </div>
                                                         <div class="mb-3">
-                                                            <label class="form-label" for="selectParentAdd">Parent</label>
-                                                            <select class="form-select parent-select" id="selectParentAdd" aria-label="Default select example">
+                                                            <label class="form-label"
+                                                                   for="selectParentAdd">Parent</label>
+                                                            <select class="form-select parent-select"
+                                                                    id="selectParentAdd"
+                                                                    aria-label="Default select example">
                                                             </select>
                                                         </div>
                                                         <button type="submit" class="btn btn-primary">Add</button>
                                                     </form>
                                                 </div>
-
-
                                             </div>
                                             <div class="tab-pane container fade p-0" id="tab-edit">
-
-
                                                 <div class="mt-4">
                                                     <h2>Edit a node or a leaf</h2>
                                                     <form id="editCategoryForm" class="needs-validation">
@@ -65,38 +66,54 @@
                                                         <div class="mb-3">
                                                             <input type="hidden" id="inputIdCategoryEdit">
                                                             <label class="form-label" for="inputNameEdit">Name</label>
-                                                            <input type="text" class="form-control" id="inputNameEdit" placeholder="Name" required>
-                                                            <div class="invalid-feedback">Please enter a valid email address.</div>
+                                                            <input type="text" class="form-control" id="inputNameEdit"
+                                                                   placeholder="Name" required>
+                                                            <div class="invalid-feedback">Please enter a valid email
+                                                                address.
+                                                            </div>
                                                         </div>
                                                         <div class="mb-3">
-                                                            <label class="form-label" for="selectParentEdit">Parent</label>
-                                                            <select class="form-select parent-select" id="selectParentEdit" aria-label="Default select example">
+                                                            <label class="form-label"
+                                                                   for="selectParentEdit">Parent</label>
+                                                            <select class="form-select parent-select"
+                                                                    id="selectParentEdit"
+                                                                    aria-label="Default select example">
 
                                                             </select>
                                                         </div>
-                                                        <button type="submit" id="editCategory" class="btn btn-primary">Edit</button>
+                                                        <button type="submit" id="editCategory" class="btn btn-primary">
+                                                            Edit
+                                                        </button>
                                                     </form>
                                                 </div>
-
-
                                             </div>
                                             <div class="tab-pane container fade p-0" id="tab-delete">
-
                                                 <div class="mt-4">
                                                     <h2>Select a node or a leaf to remove </h2>
                                                     <form id="deleteCategoryForm" class="needs-validation">
                                                         <div class="mb-3">
                                                             <input type="hidden" id="inputIdCategoryDelete">
                                                             <label class="form-label" for="readonlyInputNameForDelete">Name</label>
-                                                            <input type="text" class="form-control" id="readonlyInputNameForDelete" value="Name..." aria-label="Name..." readonly>
-                                                            <div class="invalid-feedback">Please enter a valid name.</div>
+                                                            <input type="text" class="form-control"
+                                                                   id="readonlyInputNameForDelete" value="Name..."
+                                                                   aria-label="Name..." readonly>
+                                                            <div class="invalid-feedback">Please enter a valid name.
+                                                            </div>
                                                         </div>
                                                         <div class="mb-3">
-                                                            <label class="form-label" for="readonlyInputParentForDelete">Parent</label>
-                                                            <input type="text" class="form-control" id="readonlyInputParentForDelete" value="Parent name..." aria-label="Parent name..." readonly>
-                                                            <div class="invalid-feedback">Please enter a valid email address.</div>
+                                                            <label class="form-label"
+                                                                   for="readonlyInputParentForDelete">Parent</label>
+                                                            <input type="text" class="form-control"
+                                                                   id="readonlyInputParentForDelete"
+                                                                   value="Parent name..." aria-label="Parent name..."
+                                                                   readonly>
+                                                            <div class="invalid-feedback">Please enter a valid email
+                                                                address.
+                                                            </div>
                                                         </div>
-                                                        <button type="submit" id="deleteCategory" class="btn btn-primary">Delete</button>
+                                                        <button type="submit" id="deleteCategory"
+                                                                class="btn btn-primary">Delete
+                                                        </button>
                                                     </form>
                                                 </div>
 
